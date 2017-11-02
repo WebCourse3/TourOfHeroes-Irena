0007df2b46fd0d7c5e8cf0606a145a448099d1ff
Irena <ire95na@gmail.com>
2017-11-02 14:00:20 +0200

    serverside

---
+++ server.js
@@ -0,0 +1,64 @@
+var heros = [{id:1, name:"irena"},
+	{id:2, name:"irena"},
+	{id:3, name:"yosi"}];
+
+var express = require('express');
+var app = express();
+
+var http = require('http').Server(app);
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.route("/heroes")
+	.get(function(req,res)
+{
+	res.send(heros);
+})
+	.post(function (req, res) {
+
+		var hero = req.body;
+		heros.push(hero);
+		res.send(heros);
+	})
+
+ .delete(function (req,res)
+	{
+		for (var i = 0; i < heros.length; i++) {
+			if (heros[i].name == req.query['name']) {
+				heros.splice(i,1);
+
+			}
+		}
+	res.send(heros);
+});
+
+
+app.route("/heroes/:id")
+	.get(function (req,res) {
+		res.send(heros[req.params.id-1]);
+	})
+	.put(function (req,res){
+		for (var i = 0; i < heros.length; i++) {
+			if (heros[i].id === req.params.id-1) {
+				heros[i].name = "emma";// req.params.name
+				break;
+			}
+		}
+		res.send(heros[i]);
+
+	})
+	.delete(function (req, res) {
+		for (var i = 0; i < heros.length; i++) {
+			if (heros[i].id == req.params.id) {
+				heros.splice(i,1);
+
+			}
+		}
+		res.send(heros)
+});
+http.listen(3000, function(){
+	console.log('listening on *:3000');
+});
+
+
